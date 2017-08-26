c397a57402e5f3c4da076386939ff00ccf35d2bc
장영찬 <kuroli777@gmail.com>
2017-08-26 14:14:11 +0900

    시간에 따라 골드 양이 늘어나는 것까지. (Singleton 기법, IEnumerator와 StartCoroutine을 이용한 코루틴 기법)

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour {
+
+    //Singleton class pattern start
+
+    static GameObject _container;
+    static GameObject Container
+    {
+        get
+        {
+            return _container;
+        }
+    }
+    static DataController _instance;
+    public static DataController Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _container = new GameObject();
+                _container.name = "DataController";
+                _instance = _container.AddComponent(typeof(DataController)) as DataController;
+                DontDestroyOnLoad(_container);
+            }
+            return _instance;
+        }
+    }
+    //Singleton class pattern end
+
+
+    public int Gold = 0;
+
+    public int GoldPerSec = 1; //how much Gold increase per second
+
+
+
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text TextGold;
+
+	// Use this for initialization
+	void Start () {
+        TextGold.text = DataController.Instance.Gold.ToString(); //숫자를 문자로 바꿔줌
+        StartCoroutine(StartCollectGold()); //코루틴 실행!
+	}
+	
+    IEnumerator StartCollectGold() //코루틴. 1초에 1번씩 ~해라 할때 or 애니메이션 or 시간순서. 비동기식. multy-thread와는 다름. 
+    {
+
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1f); //현실 시간으로 1초를 쉰다.
+            DataController.Instance.Gold += DataController.Instance.GoldPerSec;
+            TextGold.text = DataController.Instance.Gold.ToString();
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
